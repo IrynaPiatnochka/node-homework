4aafafcdb79019a638fa6dfdb321f512e91ae9d6
IrynaPiatnochka <ivpiatnochka@gmail.com>
2026-07-07 13:06:02 -0500

    Added comment callback hell with a code example

--- assignment1/async-demo.js
+++ assignment1/async-demo.js
@@ -8,6 +8,21 @@ const filePath = path.join(__dirname, 'sample-files', 'sample.txt');
 // Write a sample file for demonstration
 fs.writeFileSync(filePath, 'Hello, async world!');
 
+/*
+Callback hell example:
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (!err) {
+    fs.readFile(filePath, 'utf8', (err2, data2) => {
+      if (!err2) {
+        fs.readFile(filePath, 'utf8', (err3, data3) => {
+          console.log("Deep nesting:", data3);
+        });
+      }
+    });
+  }
+});
+*/
 
 // 1. Callback style
 fs.readFile(filePath, 'utf8', (err, data) => {
